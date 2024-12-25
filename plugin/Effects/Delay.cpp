64311b7bbb210669d41d54e32bd61b4e2fcbb43b
Jens W. Langenberg <jerry2912@icloud.com>
2024-12-25 15:41:28 +0100

    Started to create blueprint for delay

--- plugin/Effects/Delay.cpp
+++ plugin/Effects/Delay.cpp
@@ -8,3 +8,45 @@
 */
 
 #include "Delay.h"
+
+
+Delay::Delay()
+{
+}
+
+
+void Delay::prepare(juce::dsp::ProcessSpec &spec, float maxDelayInMS)
+{
+	// Store the parameters locally
+	mSampleRate					= spec.sampleRate;
+	mNumChannels				= spec.numChannels;
+	mMaxBlockSize				= spec.maximumBlockSize;
+
+	// prepare the buffer
+	const int maxDelayInSamples = (int)std::ceil(maxDelayInMS * 1000 * mSampleRate);
+
+	mDelayBuffer.prepare(mSampleRate, mMaxBlockSize, mNumChannels, ((int)maxDelayInMS * 1000));
+
+	mCircularBufferLength = mDelayBuffer.getBuffer().getNumSamples();
+
+	mWritePosition = 0;
+}
+
+
+void Delay::process(juce::AudioBuffer<float> &buffer)
+{
+	const int numSamples = buffer.getNumSamples();
+	const int numChannels = buffer.getNumChannels();
+
+	mDelayInSamples		  = (int)(mDelayTimeMS.getCurrentValue() * 1000 * mSampleRate);
+
+	// For each channel we will
+	// 1. Write the incoming samples into the delay buffer
+	// 2. Read out delayed samples from the delay buffer
+	// 3. Apply feedback
+	// 4. Mix into output
+
+
+}
+
+
--- plugin/Effects/Delay.h
+++ plugin/Effects/Delay.h
@@ -9,7 +9,38 @@
 
 #pragma once
 
+#include <juce_dsp/juce_dsp.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+
+#include "CircularBuffer.h"
+
 
 class Delay
 {
+public:
+	Delay();
+
+	void prepare(juce::dsp::ProcessSpec &spec, float maxDelayInMS);
+
+
+	void process(juce::AudioBuffer<float> &buffer);
+
+
+private:
+	juce::SmoothedValue<float> mDelayTimeMS			 = 500;	 // dummy parameters for now
+	juce::SmoothedValue<float> mFeedback			 = 0.3f; // dummy parameters for now
+	juce::SmoothedValue<float> mMix					 = 0.5f; // dummy parameters for now
+
+	int						   mDelayInSamples		 = 0;	 // how many samples the read pointer will be offset (delayTime in Seconds * sampleRate)
+
+
+
+	int						   mWritePosition		 = 0;
+	int						   mCircularBufferLength = 0;
+
+	double					   mSampleRate			 = 48000;
+	int						   mNumChannels			 = 0;
+	int						   mMaxBlockSize		 = 0;
+
+	CircularBuffer			   mDelayBuffer;
 };
