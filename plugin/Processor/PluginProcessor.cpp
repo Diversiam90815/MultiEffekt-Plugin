190781ac095326b00eae45bc0f481f0d2abffde8
Jens W. Langenberg <jerry2912@icloud.com>
2024-12-29 20:53:15 +0100

    Simplified processor code

--- plugin/Processor/PluginProcessor.cpp
+++ plugin/Processor/PluginProcessor.cpp
@@ -12,17 +12,7 @@
 
 
 PluginProcessor::PluginProcessor()
-#ifndef JucePlugin_PreferredChannelConfigurations
-	: AudioProcessor(BusesProperties()
-#if !JucePlugin_IsMidiEffect
-#if !JucePlugin_IsSynth
-						 .withInput("Input", juce::AudioChannelSet::stereo(), true)
-#endif
-						 .withOutput("Output", juce::AudioChannelSet::stereo(), true)
-#endif
-						 ),
-	  mValueTreeState(*this, nullptr, "PARAMETERS", createParameterLayout())
-#endif
+	: AudioProcessor(BusesProperties().withInput("Input", juce::AudioChannelSet::stereo(), true).withOutput("Output", juce::AudioChannelSet::stereo(), true)), mValueTreeState(*this, nullptr, "PARAMETERS", createParameterLayout())
 {
 	mValueTreeState.addParameterListener(paramInput, this);
 	mValueTreeState.addParameterListener(paramOutput, this);
@@ -77,33 +67,22 @@ const juce::String PluginProcessor::getName() const
 	return JucePlugin_Name;
 }
 
+
 bool PluginProcessor::acceptsMidi() const
 {
-#if JucePlugin_WantsMidiInput
-	return true;
-#else
 	return false;
-#endif
 }
 
 
 bool PluginProcessor::producesMidi() const
 {
-#if JucePlugin_ProducesMidiOutput
-	return true;
-#else
 	return false;
-#endif
 }
 
 
 bool PluginProcessor::isMidiEffect() const
 {
-#if JucePlugin_IsMidiEffect
-	return true;
-#else
 	return false;
-#endif
 }
 
 
@@ -166,25 +145,16 @@ void PluginProcessor::releaseResources()
 }
 
 
-#ifndef JucePlugin_PreferredChannelConfigurations
 bool PluginProcessor::isBusesLayoutSupported(const BusesLayout &layouts) const
 {
-#if JucePlugin_IsMidiEffect
-	ignoreUnused(layouts);
-	return true;
-#else
 	if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
 		return false;
 
-#if !JucePlugin_IsSynth
 	if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
 		return false;
-#endif
 
 	return true;
-#endif
 }
-#endif
 
 
 void PluginProcessor::processBlock(juce::AudioBuffer<float> &buffer, juce::MidiBuffer &midiMessages)
--- plugin/Processor/PluginProcessor.h
+++ plugin/Processor/PluginProcessor.h
@@ -17,73 +17,53 @@
 #include "Delay.h"
 #include "PannerManager.h"
 
-/*
-#include "MonoPanner.h"
-#include "StereoPanner.h"*/
-
 
 class PluginProcessor : public juce::AudioProcessor, public juce::AudioProcessorValueTreeState::Listener
 {
 public:
-	//==============================================================================
-	//						CONSTRUCTOR - DESTRUCTOR
-	//==============================================================================
-
 	PluginProcessor();
-
 	~PluginProcessor() override;
 
-
-	//==============================================================================
-	//						PUBLIC METHODS
-	//==============================================================================
 
 	juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
 
 	void												parameterChanged(const juce::String &parameterID, float newValue) override;
 
-
-	//==============================================================================
-	//						PUBLIC METHODS - JUCE OVERRIDES
-	//==============================================================================
-
 	void												prepareToPlay(double sampleRate, int samplesPerBlock) override;
 
 	void												releaseResources() override;
 
-#ifndef JucePlugin_PreferredChannelConfigurations
-	bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
-#endif
+	bool												isBusesLayoutSupported(const BusesLayout &layouts) const override;
 
-	void						processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
+	void												processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
 
-	juce::AudioProcessorEditor *createEditor() override;
+	juce::AudioProcessorEditor						   *createEditor() override;
 
-	bool						hasEditor() const override;
+	bool												hasEditor() const override;
 
-	const juce::String			getName() const override;
+	const juce::String									getName() const override;
 
-	bool						acceptsMidi() const override;
+	bool												acceptsMidi() const override;
 
-	bool						producesMidi() const override;
+	bool												producesMidi() const override;
 
-	bool						isMidiEffect() const override;
+	bool												isMidiEffect() const override;
 
-	double						getTailLengthSeconds() const override;
+	double												getTailLengthSeconds() const override;
 
-	int							getNumPrograms() override;
+	int													getNumPrograms() override;
 
-	int							getCurrentProgram() override;
+	int													getCurrentProgram() override;
 
-	void						setCurrentProgram(int index) override;
+	void												setCurrentProgram(int index) override;
 
-	const juce::String			getProgramName(int index) override;
+	const juce::String									getProgramName(int index) override;
 
-	void						changeProgramName(int index, const juce::String &newName) override;
+	void												changeProgramName(int index, const juce::String &newName) override;
 
-	void						getStateInformation(juce::MemoryBlock &destData) override;
+	void												getStateInformation(juce::MemoryBlock &destData) override;
 
-	void						setStateInformation(const void *data, int sizeInBytes) override;
+	void												setStateInformation(const void *data, int sizeInBytes) override;
 
 
 private:
@@ -97,10 +77,6 @@ private:
 
 	void							   updatePannerParameter();
 
-
-	//==============================================================================
-	//						PRIVATE OBJECTS
-	//==============================================================================
 
 	Distortion<float>				   mDistortionModule;
 
