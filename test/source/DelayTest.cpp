c77a030f764419ec0966be4f3017fc0ea7cb62e8
Jens W. Langenberg <jerry2912@icloud.com>
2024-12-31 16:17:20 +0100

    Added Delay Test (change of type)

--- test/source/DelayTest.cpp
+++ test/source/DelayTest.cpp
@@ -1,3 +1,11 @@
 #include <gtest/gtest.h>
 
 #include "PluginProcessor.h"
+
+TEST(Delay, DelayTypeChange)
+{
+	Delay<float> delay;
+	delay.setDelayType(DelayType::PingPong);
+
+	ASSERT_EQ(delay.getDelayType(), DelayType::PingPong);
+}
