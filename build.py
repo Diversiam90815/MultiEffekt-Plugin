1aefb850cf8fe0085a4d6cb323b514f541344ffb
Jens Langenberg <jens.langenberg@corsair.com>
2025-02-24 21:12:15 +0100

    Added versioning and VS platform to build script

--- build.py
+++ build.py
@@ -1,11 +1,11 @@
 import argparse
 import os
 import sys
+import shutil 
+import re
 
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, check_output
 
-
-PLATFORM_GENERATOR = '\"Visual Studio 17\"'
 
 
 class AutoCWD(object):
@@ -24,22 +24,16 @@ class BuildRunner(object):
     TARGET_CONFIG = 'Release'
 
     def __init__(self):   
-        formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position=100, width=200)
-        description = 'This tool can be used to build and deploy the Distortion project.'
-        epilog = ''
-        parser = argparse.ArgumentParser(description=description, formatter_class=formatter_class, epilog=epilog)
+        parser = argparse.ArgumentParser(description='This tool can be used to build and deploy the project.')
         parser.add_argument('-p', '--prepare', action='store_true', help='prepares the project for use with IDE')
         parser.add_argument('-d', '--debug', action='store_true', help='prepare or build debug version')
         parser.add_argument('-b', '--build', action='store_true', help='build the project')
         parser.add_argument('-v', '--version', action='store_true', help='display Python and CMake versions')
 
-
-        self.args = parser.parse_args()
-       
+        self.args = parser.parse_args()       
         self.version = ""
-
-        path_project = os.path.dirname(os.path.realpath(__file__))
-        self.args.path_project = path_project
+        self.platform = self._find_latest_visual_studio_version()
+        self.args.path_project = os.path.dirname(os.path.realpath(__file__))
 
 
     @staticmethod
@@ -75,13 +69,93 @@ class BuildRunner(object):
 
 
     def _print_versions(self):
-        """ Print Python and CMake versions """
-        print("Python version:", sys.version)
+        """ Print Python, CMake & Projects versions """
+        self._update_app_version()
+        print(f"Project's version: {self.version}")
+        print(f"Python version: {sys.version}")
         try:
             cmake_version = self._execute_command("cmake --version", "Get CMake version")
-            print("CMake version:", cmake_version.splitlines()[0])  # print only the first line of output
+            print(f"CMake version: {cmake_version.splitlines()[0]}")  # print only the first line of output
         except Exception as e:
             print(f"Error retrieving CMake version: {e}")
+
+
+    def _find_latest_visual_studio_version(self):
+        vswhere = os.path.join(os.environ.get("ProgramFiles(x86)"), "Microsoft Visual Studio", "Installer", "vswhere.exe")
+
+        if not os.path.exists(vswhere):
+            print("vswhere.exe not found! Using Visual Studio 2022 as default")
+            return '\"Visual Studio 17\"'
+
+        result = check_output([vswhere, '-latest', '-property', 'installationVersion'], shell=True).decode().strip()
+        versionMatch = re.match(r'^(\d+)', result)
+
+        if versionMatch:
+            vsVersion = versionMatch.group(1)
+            return f'\"Visual Studio {vsVersion}\"'
+
+        else:
+            print("Could not determine latest Visual Studio version. Using Visual Studio 2022 as default")
+            return '\"Visual Studio 17\"'
+
+
+    def _get_number_of_commits(self):
+        autoCWD = AutoCWD(self.args.path_project)
+        sys.stdout.flush()
+        commit_hashes = check_output("git rev-list HEAD", shell=True).rstrip()
+        commit_number = commit_hashes.count(b'\n') + 1
+        del autoCWD
+        return commit_number
+
+
+    def _get_current_project_build_number(self):
+        BuildNumber = self._get_number_of_commits()
+        return BuildNumber
+    
+
+    def _get_current_project_version(self):
+        cmake_file_path = os.path.join(self.args.path_project, "plugin", "CMakeLists.txt")
+        pattern = r'set\(PLUGIN_VERSION\s+(\d+\.\d+\.\d+)\)'
+
+        with open(cmake_file_path, 'r') as f:
+            for line in f:
+                match = re.search(pattern, line)
+                if match:
+                    return match.group(1)
+        return None
+
+
+    def _update_app_version_in_cmake(self, project_version):
+        pattern = r'set\(PLUGIN_VERSION\s+(\d+\.\d+\.\d+)\)'
+        cmakeFile = os.path.join(self.args.path_project, "plugin", "CMakeLists.txt")
+        tmpFile = cmakeFile + '.tmp'
+
+        with open (cmakeFile, 'r') as fileIn, open (tmpFile, 'w') as fileOut:
+            for line in fileIn:
+                match = re.search(pattern, line)
+                if match:
+                    fileOut.write(f'set(PLUGIN_VERSION {project_version})\n')
+                else:
+                    fileOut.write(line)
+
+        shutil.move(tmpFile, cmakeFile)
+        fileIn.close()
+        fileOut.close()
+
+
+    def _update_app_version(self):
+        build_number = self._get_current_project_build_number()
+        current_version = self._get_current_project_version()
+
+        if current_version:
+            version_parts = current_version.split('.')
+            version_parts[-1] = str(build_number)
+            self.version = '.'.join(version_parts)
+            self._update_app_version_in_cmake(self.version)
+            print(f"Set project version to {self.version}!")
+
+        else:
+            print("No version attribute found!")
 
 
 
@@ -89,7 +163,7 @@ class BuildRunner(object):
         projectfolderVS =  os.path.join(self.args.path_project)
         autoCWD = AutoCWD(projectfolderVS)
 
-        prepare_cmd = f'cmake -G {PLATFORM_GENERATOR} -B build'
+        prepare_cmd = f'cmake -G {self.platform} -B build'
         self._execute_command(prepare_cmd, "Select build generator")
         
         del autoCWD
@@ -113,8 +187,10 @@ class BuildRunner(object):
             BuildRunner.TARGET_CONFIG = 'Debug'
         if self.args.prepare:
             self._build_prepare()
+            self._update_app_version()
         if self.args.build:
             self._build_prepare()
+            self._update_app_version()
             self._build_project()
 
             
